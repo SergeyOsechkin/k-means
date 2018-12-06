32b06b6e0651186f2883f439b608695361cecb44
Osechkin Sergey <osechkin.s.v@gmail.com>
2018-12-07 00:02:13 +0300

    don't work

--- app/means-shift.py
+++ app/means-shift.py
@@ -1,7 +1,6 @@
 import numpy as np
-import os
 from PIL import Image
-from sklearn.cluster import MeanShift
+from sklearn.cluster import mean_shift
 
 
 if __name__ == '__main__':
@@ -10,11 +9,7 @@ if __name__ == '__main__':
     path_file = '../drosophila_kc167_1_images/CPvalid1_48_40x_Tiles_p0003DAPI.TIF'
     img = Image.open(path_file).convert('RGB')
     pixels = np.float32(img).reshape(-1, 3)
-    ms = MeanShift()
-    ms.fit(pixels)
-    label = ms.labels_
-    center = ms.cluster_centers_
+    center, label = mean_shift(pixels, max_iter=1)
     center = np.uint8(center)
     res = center[label].reshape((512, 512, 3))
-    Image.fromarray(res, 'RGB').save(dir_to + '/qwe.bmp')
-
+    Image.fromarray(res, 'RGB').show()
